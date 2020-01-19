5fa234abb916d7d583eb7ac36f18cd328e21601b
unknown <muratgumus05@gmail.com>
2020-01-19 18:58:28 +0300

    Generating component

---
+++ MovieApp/src/app/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'movies',
+    templateUrl:'movies.component.html'
+    // template: '<h2>Movies</h2>'
+})
+export class MoviesComponent {
+
+}
