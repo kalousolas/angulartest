e287e49267fd0aba787012786c21bd07db0feb5b
unknown <muratgumus05@gmail.com>
2020-01-30 16:05:58 +0300

    movieapp

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+//import { Movies } from '../movie.datasource';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
     selector: 'movies',
-    templateUrl:'movies.component.html'
-    // template: '<h2>Movies</h2>'
+    templateUrl: 'movies.component.html',
+    //styles: ['h2 {color:blue;}']
+    // template: '<h2>{{"Title: " + getTitle()}}</h2> <div>{{movie.id}}</div> <div>{{movie.name}}</div> '
 })
 export class MoviesComponent {
+    title = 'Movies List';
+    // movie: Movie = {
+    //     id: 1,
+    //     name: 'Movie Name'
+    // }
 
+    // movies = ['movie 1', 'movie 2', 'movie 3'];
+    movies: Movie[];
+    selectedMovie: Movie;
+
+    constructor(private movieService: MovieService) {
+
+    }
+    ngOnInit(): void {
+        this.getMovies();
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    onSelect(movie: Movie): void {
+        this.selectedMovie = movie;
+    }
+
+    getMovies(): void {
+        this.movieService.getMovies().subscribe(movies => {
+            this.movies = movies;
+        });
+    }
 }
