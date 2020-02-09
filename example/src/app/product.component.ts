5ffc9055cf4eb145f9fe79fdf6b19bad96e76bc8
unknown <muratgumus05@gmail.com>
2020-02-09 18:12:06 +0300

    pipes

--- example/src/app/product.component.ts
+++ example/src/app/product.component.ts
@@ -6,14 +6,21 @@ import { Product } from './product.model';
 @Component({
     selector: "app",
     //templateUrl: "product.component.html",
+    templateUrl: "product.component.pipes.html",
     //template:'<input [value]="email" (keyup.enter)="email=$event.target.value; onKeyUp()"/>',
-    template: '<input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/> <br> <span>{{email}}</span>',
+    //template: '<input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/> <br> <span>{{email}}</span>',
     styleUrls: ["product.component.css"]
 })
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
     disabled = true;
     email = "muratgumus05@gmail.com";
+    today: number = Date.now();
+    title: string = 'Murat';
+    students: number = 132448;
+    price: number = 15.99;
+    completed: number = 87.45;
+    text: string = 'Angular Currency Pipe is one of the bulit in pipe in Angular used to format currency value according to given country code,currency,decimal,locale information.';
     //product: Product = this.model.getProductById(1);
 
     getClasses(id: number): string {
